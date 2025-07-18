b56952a7aff1e6a8c001017fa643a95a17a4993c
CHUO Kimheng <hengk7401@gmail.com>
2025-07-18 15:35:48 +0000

    Upgrade feature PDF Transcript when transcripts follow Original document

--- .next/static/chunks/src_app_pdf-transcript_page_tsx_c922527c._.js
+++ .next/static/chunks/src_app_pdf-transcript_page_tsx_c922527c._.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_lib_client-export_ts_80cb30cf._.js",
+  "static/chunks/src_lib_client-export_ts_9de825e4._.js",
   "static/chunks/node_modules_14e26320._.js",
-  "static/chunks/src_0c5cf9b2._.js"
+  "static/chunks/src_cc5d15de._.js"
 ],
     source: "dynamic"
 });
