c427531e3760d2758439f36cf33be4c9705c5e28
CHUO Kimheng <hengk7401@gmail.com>
2025-07-18 12:30:25 +0000

    GenkitError: INVALID_ARGUMENT: Must supply a `model` to `generate()` cal

--- .next/static/chunks/src_app_pdf-transcript_page_tsx_c922527c._.js
+++ .next/static/chunks/src_app_pdf-transcript_page_tsx_c922527c._.js
@@ -1,9 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_cffc67b6._.js",
-  "static/chunks/src_e2464705._.js",
-  "static/chunks/[root-of-the-server]__dcdbfebe._.css"
+  "static/chunks/node_modules_docx_build_index_mjs_988117c9._.js",
+  "static/chunks/node_modules_b6796cba._.js",
+  "static/chunks/src_4bdd54b6._.js"
 ],
     source: "dynamic"
 });
